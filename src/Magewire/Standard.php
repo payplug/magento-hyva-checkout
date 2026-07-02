d67e8e9ac769b9f1bf0efab269f16624f6aa1c6a
Benoit Alix <benoit.alix@dnd.fr>
2026-07-02 10:54:41 +0200

    MAG-636: Add Hosted Fields payment mode

--- src/Magewire/Standard.php
+++ src/Magewire/Standard.php
@@ -19,5 +19,21 @@ class Standard extends Main
         $quote->getPayment()->setAdditionalInformation('payplug_payments_customer_card_id', $card_id);
         $quote->getPayment()->save();
     }
+
+    public function setHostedFieldsAdditionalData(
+        string $token,
+        string $brand,
+        bool $saveCard,
+        string $cardHolder
+    ): void {
+        $payment = $this->sessionCheckout->getQuote()->getPayment();
+        $payment->unsAdditionalInformation('payplug_payments_customer_card_id');
+        $payment->setAdditionalInformation('payplug_hosted_fields_payment', true);
+        $payment->setAdditionalInformation('payplug_hosted_fields_token', $token);
+        $payment->setAdditionalInformation('payplug_hosted_fields_brand', $brand);
+        $payment->setAdditionalInformation('payplug_hosted_fields_save_card', $saveCard);
+        $payment->setAdditionalInformation('payplug_hosted_fields_card_holder', $cardHolder);
+        $payment->save();
+    }
 }
 
