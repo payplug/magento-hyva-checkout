7c8f451e7424d4ec2cb0409b6437770068f5d8c1
Benoit Alix <benoit.alix@dnd.fr>
2026-08-24 16:30:55 +0200

    release/v4.0.2: Add configuration fallback for Tailwind V3

--- src/view/frontend/tailwind/tailwind.config.js
+++ src/view/frontend/tailwind/tailwind.config.js
@@ -2,7 +2,7 @@
  * Payplug - https://www.payplug.com/
  * Copyright © Payplug. All rights reserved.
  * See LICENSE for license details.
- * 
+ *
  * Fallback configuration (only for Tailwind V3)
  */
 
