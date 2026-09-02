7ceaab592a5063706f7b8534beaa57b46877a797
Frédéric TISSOT <contact@espritdev.fr>
2026-09-02 16:50:13 +0200

    MAG-636: Update method signatures and naming for HF additional data handling

--- src/Magewire/Standard.php
+++ src/Magewire/Standard.php
@@ -9,24 +9,37 @@ declare(strict_types=1);
 
 namespace Payplug\PaymentsHyvaCheckout\Magewire;
 
+use Exception;
 use Hyva\Checkout\Magewire\Main;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Payplug\Payments\Api\Data\OrderPaymentInterface;
 
 class Standard extends Main
 {
-    public function setAdditionalData(string $card_id)
+    /**
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     * @throws Exception
+     */
+    public function setCardIdOnAdditionalData(string $card_id): void
     {
         $quote = $this->sessionCheckout->getQuote();
         $quote->getPayment()->setAdditionalInformation('payplug_payments_customer_card_id', $card_id);
         $quote->getPayment()->save();
     }
 
+    /**
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     * @throws Exception
+     */
     public function setHostedFieldsAdditionalData(
-        string $cardId,
         string $token,
         string $brand,
         bool $saveCard,
-        string $cardHolder
+        string $cardHolder,
+        ?string $cardId = null
     ): void {
         $payment = $this->sessionCheckout->getQuote()->getPayment();
         $payment->setAdditionalInformation(OrderPaymentInterface::HF_CARD_ID_KEY, $cardId);
